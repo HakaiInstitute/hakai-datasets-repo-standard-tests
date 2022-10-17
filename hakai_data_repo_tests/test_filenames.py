309cfdacfb2785961840d5f8b718334459735692
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2022-10-17 16:18:34 -0400

    add .fileignore and config.yaml method to setup parameters for each datasets

--- hakai_data_repo_tests/test_filenames.py
+++ hakai_data_repo_tests/test_filenames.py
@@ -1,41 +1,30 @@
 import logging
 import unittest
-from glob import glob
 import re
+import os
+
+from . import utils
 
 logger = logging.getLogger(__name__)
 
-file_extensions = [".csv", ".txt", ".MON"]
-ignore_files = [
-    "./instrument-log.csv",
-    "./station-log.csv",
-    "./stations-log.csv",
-    "./requirements.txt",
-]
-ignore_file_regex = re.compile()
-hakai_name_convention = re.compile(
-    r"(?P<station>[^\\\/]+)_(?P<serial_number>\d+)_(?P<retrial_date>\d{8})(_rawdata){0,1}\..{3}$"
-)
-
+config = utils.read_data_repo_config()
 
 class TestFileNameConvention(unittest.TestCase):
-    def test_hakai_filename_convention(self):
-
+    def test_filename_convention(self):
         """Review Hakai File Name Convention"""
+        convention_expression = config.get('file_naming_convention')
+        if convention_expression is None:
+            logger.info("No file naming convention specified")
+            return
+
         # Get data file name
+        convention = re.compile(convention_expression)
         files = []
-        for extension in file_extensions:
-            files += glob(
-                f"./**/*{extension}",
-                recursive=True,
-            )
+        for pattern in config["data"]["search_pattern"]:
+            files += utils.get_file_list(pattern, file_ignore=config["file_ignore"],recursive=config["data"]["recursive"])
 
         assert len(files) != 0, "Failed to detect any files in repository"
-
-        # Ignore files
-        files = [file for file in files if file not in ignore_files]
-
-        bad_files = [file for file in files if not hakai_name_convention.search(file)]
+        bad_files = [file for file in files if not convention.fullmatch(os.path.basename(file))]
         assert (
-            bad_files is None
+            len(bad_files)==0
         ), f"The following files do no follow the hakai name convention: {bad_files}"
