b7d9d0c52621afe8b8d0ae4b35838139567626f2
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-27 13:00:22 -0400

    add test for matching files

---
+++ tests/test_repo.py
@@ -0,0 +1,55 @@
+import pytest
+
+from hakai_data_repo_tests import test_matching_files
+
+@pytest.fixture
+def config():
+    return {
+        "file_naming_convention": ".*",
+        "data": {
+            "search_pattern": ["*"],
+            "recursive": False
+        },
+        "file_ignore": []
+    }
+
+@pytest.fixture
+def dir():
+    return "test_repo"
+
+class TestMatchingFiles:
+    def test_sucessfull_matching_file_check(self, config, tmp_path):
+        # Add tmp files
+        tmp_path.joinpath("file1.txt").touch()
+        tmp_path.joinpath("file1.csv").touch()
+        # set config
+        config = {
+            "matched_files": [
+                {
+                    "files": f"{tmp_path}/*",
+                    "pattern": ".txt",
+                    "replace": ".csv"
+                }
+            ]
+        }
+        test_matching_files.test_matching_files_check(config)
+    
+    def test_fail_matching_file_check(self, config, tmp_path):
+        # Add tmp files
+        tmp_path.joinpath("file1.txt").touch()
+        # set config
+        config = {
+            "matched_files": [
+                {
+                    "files": f"{tmp_path}/*",
+                    "pattern": ".txt",
+                    "replace": ".csv"
+                }
+            ]
+        }
+        with pytest.raises(AssertionError):
+            test_matching_files.test_matching_files_check(config)
+
+
+        
+    
