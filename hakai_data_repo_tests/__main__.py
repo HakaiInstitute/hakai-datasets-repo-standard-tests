3e2b70774facaf57353c1abfef145f98340a3371
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-28 15:09:44 -0400

    try to ouput junit-xml report

--- hakai_data_repo_tests/__main__.py
+++ hakai_data_repo_tests/__main__.py
@@ -8,7 +8,7 @@ import pytest
 logger = logging.getLogger(__name__)
 
 
-def main(dir=".", config_path="config.yaml", log_level="INFO"):
+def main(dir=".", config_path="config.yaml", log_level="INFO", junit_xml=None):
     logger.info(f"Running tests with arguments: {dir=}, {config_path=}, {log_level=}")
     return pytest.main(
         args=[
@@ -19,6 +19,7 @@ def main(dir=".", config_path="config.yaml", log_level="INFO"):
             f"--dir={dir}",
             f"--config-path={config_path}",
         ]
+        + (["--junit-xml", junit_xml] if junit_xml else [])
     )
 
 
@@ -43,7 +44,12 @@ if __name__ == "__main__":
         action="store",
         help="Log level (default: INFO)",
     )
+    parser.add_argument(
+        "--junit-xml",
+        action="store",
+        help="Create junit-xml style report",
+    )
     args = parser.parse_args()
     logging.basicConfig(level=args.log_level)
-    result = main(args.dir, args.config_path, log_level=args.log_level)
+    result = main(args.dir, args.config_path, log_level=args.log_level, junit_xml=args.junit_xml)
     sys.exit(result)
