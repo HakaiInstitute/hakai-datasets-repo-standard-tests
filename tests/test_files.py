dfcb6edf3a7f6ae08853fcc1c9a287f651ff3cbf
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-28 14:44:37 -0400

    isort black

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,7 +1,8 @@
-from hakai_data_repo_tests.__main__ import main
-
 import pytest
 import yaml
+
+from hakai_data_repo_tests.__main__ import main
+
 
 @pytest.fixture(scope="function")
 def config():
@@ -9,17 +10,17 @@ def config():
         "file_naming_convention": ".*",
         "data": {"search_pattern": ["*"], "recursive": False},
         "file_ignore": [],
-   }
+    }
 
 
 def test_file_ignore_files(config, tmp_path):
     # Add tmp files
     tmp_path.joinpath("file1.txt").touch()
     tmp_path.joinpath("file1.csv").touch()
-    tmp_path.joinpath(".fileignore").write_text("file1.csv")
+    tmp_path.joinpath(".fileignore").write_text("file1.csv\nconfig.yaml")
 
     # set config
     config["file_naming_convention"] = "file1.txt"
-    (tmp_path / "config.yaml").write_text(yaml.dumps(config))
+    (tmp_path / "config.yaml").write_text(yaml.dump(config))
     result = main(dir=tmp_path)
     assert result == 0
